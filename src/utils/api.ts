cd7618351cd6318e1dbab03e40fc9eaabb22241e
“mishan843” <mishansavaliya843@gmail.com>
2025-02-05 21:27:05 +0530

    apiurl change

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,14 +1,12 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosError } from 'axios';
 
-// const API_URL = import.meta.env.VITE_APP_API_LOCAL_URL;
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://backend-prac.onrender.com/api";
 
 const api: AxiosInstance = axios.create({
     baseURL: API_URL,
     maxContentLength: Infinity,
 });
 
-// Add a request interceptor
 api.interceptors.request.use(
     (config: InternalAxiosRequestConfig) => {
         const token = localStorage.getItem('token');
@@ -22,16 +20,12 @@ api.interceptors.request.use(
     }
 );
 
-// Add a response interceptor
 api.interceptors.response.use(
     (response) => {
-        // Handle successful responses
         return response;
     },
     (error: AxiosError) => {
-        // Handle errors
         if (error.response?.status === 401) {
-            // Handle unauthorized error
             console.log('Unauthorized');
         }
         return Promise.reject(error);
